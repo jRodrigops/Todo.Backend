4d3cfa69e7deb99d617ac4f84e7ab9468a9308a3
Rodrigo <rodrigoportela664@gmail.com>
2024-07-03 20:35:02 -0300

    feat: Configurada classe program e appsettings com co0nfiguração do banco

--- Todo.Backend/Program.cs
+++ Todo.Backend/Program.cs
@@ -1,11 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Todo.Backend.Context;
+using Todo.Backend.DependencyInjection;
+using Todo.Backend.Mapper;
+using Todo.Backend.Repositories;
+using Todo.Backend.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/Sswashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
+
+builder.Services.AddScoped<IToDoTaskRepository, ToDoTaskRepository>();
+builder.Services.AddScoped<IToDoTaskService, ToDoTaskService>();
+
+string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<TodoDbContext>(option => option.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
+
 
 var app = builder.Build();
 
