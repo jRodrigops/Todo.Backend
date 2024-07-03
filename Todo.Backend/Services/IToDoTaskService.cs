37ba16f26c5761d8cbc900b51aee5484a22f0769
Rodrigo <rodrigoportela664@gmail.com>
2024-07-03 20:31:55 -0300

    feat: Criado Service para realizar a logica e validações no codigo

---
+++ Todo.Backend/Services/IToDoTaskService.cs
@@ -0,0 +1,12 @@
+﻿using Todo.Backend.DTOs;
+
+namespace Todo.Backend.Services;
+
+public interface IToDoTaskService 
+{
+    Task<CreateToDoTaskResponseDTO> CreateTask(CreateToDoTaskRequestDTO requestDto);
+    Task<List<ListToDoTaskResponseDTO>> GetAllTasks();
+    Task<UpdateDescriptionToDoTaskResponseDTO> UpdateDescription(UpdateDescriptionToDoTaskRequestDTO request);
+    Task<UpdateStatusToDoTaskResponseDTO> UpdateStatus(UpdateStatusToDoTaskRequestDTO requestDto);
+    Task DeleteTask(DeleteToDoTaskRequestDTO requestDto);
+}
--- Todo.Backend/Services/ToDoTaskService.cs
+++ Todo.Backend/Services/ToDoTaskService.cs
@@ -1,35 +1,103 @@
 ﻿using AutoMapper;
 using System.Runtime.CompilerServices;
+using Todo.Backend.Context;
 using Todo.Backend.DTOs;
 using Todo.Backend.Models;
 using Todo.Backend.Repositories;
 
 namespace Todo.Backend.Services;
 
-public class ToDoTaskService : ToDoTaskServiceInterface
+public class ToDoTaskService : IToDoTaskService
 {
-    private readonly ToDoTaskRepository _taskRepository;
+    private readonly IToDoTaskRepository _taskRepository;
     private readonly IMapper _mapper;
+    private readonly TodoDbContext _todoDbContext;
 
-    public ToDoTaskService(IMapper mapper, ToDoTaskRepository taskRepository)
+    public ToDoTaskService(IMapper mapper, IToDoTaskRepository taskRepository, TodoDbContext todoDbContext)
     {
         _mapper = mapper;       
         _taskRepository = taskRepository;       
+        _todoDbContext = todoDbContext;     
     }
 
-    public async Task CreateTask(CreateToDoTaskRequestDTO requestDto)
+    public async Task<CreateToDoTaskResponseDTO> CreateTask(CreateToDoTaskRequestDTO requestDto)
     {
-        var newTask =  _mapper.Map<ToDoTask>(requestDto);
+        if(requestDto.Description.Length > 30)
+        {
+            throw new Exception("o campo de descrição passou de 30 caracteres");
+        }
+
+        var newTask = _mapper.Map<ToDoTask>(requestDto);
 
         await _taskRepository.AddTask(newTask);
+
+        CreateToDoTaskResponseDTO responseDto = _mapper.Map<CreateToDoTaskResponseDTO>(newTask);
+
+        return responseDto;
     }
 
     public async Task<List<ListToDoTaskResponseDTO>> GetAllTasks()
     {
-        List<ToDoTask> lisTasks = await _taskRepository.GetAll();
+        List<ToDoTask> listTasks = await _taskRepository.GetAll();
 
-        List<ListToDoTaskResponseDTO> listTaskResponseDTOs =  _mapper.Map<List<ListToDoTaskResponseDTO>>(lisTasks);
+        List<ListToDoTaskResponseDTO> listTaskResponseDTOs =  _mapper.Map<List<ListToDoTaskResponseDTO>>(listTasks);
 
         return listTaskResponseDTOs;
+    }
+
+    public async Task<UpdateDescriptionToDoTaskResponseDTO> UpdateDescription(UpdateDescriptionToDoTaskRequestDTO request)
+    {
+        ToDoTask? toDoTask = await _taskRepository.GetById(request.IdTask);
+
+        if(toDoTask == null)
+        {
+            throw new Exception("Esta Task não Existe");
+        }    
+
+        toDoTask.Description = request.Description;
+
+        _todoDbContext.SaveChanges();
+
+        UpdateDescriptionToDoTaskResponseDTO updateDescriptionToDoTaskResponse = _mapper.Map<UpdateDescriptionToDoTaskResponseDTO>(toDoTask);
+
+        return updateDescriptionToDoTaskResponse;
+    }
+
+    public async Task<UpdateStatusToDoTaskResponseDTO> UpdateStatus(UpdateStatusToDoTaskRequestDTO requestDto)
+    {
+        ToDoTask? toDoTask = await _taskRepository.GetById(requestDto.IdTask);
+
+        if (toDoTask == null)
+        {
+            throw new Exception("Esta Task não Existe");
+        }
+
+        if (toDoTask.Completed == false)
+        {
+            toDoTask.Completed = true;
+        }
+        else
+        {
+            toDoTask.Completed = false;
+        }
+        
+        _todoDbContext.SaveChanges();
+
+        UpdateStatusToDoTaskResponseDTO responseDto = _mapper.Map<UpdateStatusToDoTaskResponseDTO>(toDoTask);
+
+        return responseDto;
+    }
+
+    public async Task DeleteTask(DeleteToDoTaskRequestDTO requestDto)
+    {
+        ToDoTask? toDoTask = await _taskRepository.GetById(requestDto.idTask);
+
+        if (toDoTask == null)
+        {
+           throw new Exception("Esta Task não Existe");
+        }
+
+        _todoDbContext.Remove(toDoTask);
+        _todoDbContext.SaveChanges();
     }
 }
