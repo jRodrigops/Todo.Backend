49973551407370f039fc43d4043d52fde92fab8c
Rodrigo <rodrigoportela664@gmail.com>
2024-07-03 20:12:41 -0300

    Fix: Removido classes que nao estavm mais sendo usadas

--- Todo.Backend/Repositories/ToDoTaskRepositoryInterface.cs
+++
@@ -1,9 +0,0 @@
-﻿using Todo.Backend.Models;
-
-namespace Todo.Backend.Repositories;
-
-public interface ToDoTaskRepositoryInterface
-{
-    Task<List<ToDoTask>> GetAll();
-    Task AddTask(ToDoTask newTask);
-}
--- Todo.Backend/Services/ToDoTaskServiceInterface.cs
+++
@@ -1,9 +0,0 @@
-﻿using Todo.Backend.DTOs;
-
-namespace Todo.Backend.Services;
-
-public interface ToDoTaskServiceInterface 
-{
-    Task CreateTask(CreateToDoTaskRequestDTO requestDto);
-    Task<List<ListToDoTaskResponseDTO>> GetAllTasks();
-}
