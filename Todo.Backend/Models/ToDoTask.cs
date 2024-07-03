1994fbc53dade7e64dfe475589a627ef72442846
Rodrigo <rodrigoportela664@gmail.com>
2024-07-03 20:24:16 -0300

    feat: Criada tabela ToDoTask

--- Todo.Backend/Models/ToDoTask.cs
+++ Todo.Backend/Models/ToDoTask.cs
@@ -1,15 +1,23 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Todo.Backend.Models;
 
 public class ToDoTask
 {
+    [Key]
     [Column("id_task")]
     [Required]
-    public  int idTarefa { get; set; }
+    public int idTask { get; set; }
 
     [Column("description")]
     [Required]
+    [MinLength(3)]
+    [MaxLength(30)]
     public string Description { get; set; }
+
+    [Column("completed")]
+    public bool Completed { get; set; } = false;
 }
