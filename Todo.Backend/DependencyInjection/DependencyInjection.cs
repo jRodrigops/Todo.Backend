e695ac4e406a91abd7efd93b06de1fd7b56eca18
Rodrigo <rodrigoportela664@gmail.com>
2024-07-03 20:29:07 -0300

    feat: Criado Arquivo para realizar a injeção de depedencia da aplicação

---
+++ Todo.Backend/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Todo.Backend.Mapper;
+using Todo.Backend.Repositories;
+using Todo.Backend.Services;
+
+namespace Todo.Backend.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection Ioc(this IServiceCollection services)
+    {
+        services.AddScoped<IToDoTaskRepository, ToDoTaskRepository>();
+        services.AddScoped<IToDoTaskService, ToDoTaskService>();
+
+        services.AddAutoMapper(typeof(MappingProfile).Assembly);
+
+        return services;
+    }
+}
