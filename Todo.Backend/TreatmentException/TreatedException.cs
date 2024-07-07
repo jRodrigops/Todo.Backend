bdbccb1ecf6dd177e6f58a7e4dddae2729cc9818
Jose <jose.silva@brisabr.com.br>
2024-07-07 14:27:37 -0300

    feat: Criada uma classe para realizar o tratamento de erro

---
+++ Todo.Backend/TreatmentException/TreatedException.cs
@@ -0,0 +1,13 @@
+﻿namespace Todo.Backend.TreatmentException;
+
+public class TreatedException : Exception
+{
+    public int ErrorCode { get; set; }
+    public string ErrorDetails { get; set; }
+
+    public TreatedException(string message, int errorCode, string errorDetails) : base(message) 
+    {
+        ErrorCode = errorCode;
+        ErrorDetails = errorDetails;
+    }
+}
