497e46c0024f88740f966030545230a6ff026710
Jose <jose.silva@brisabr.com.br>
2025-11-22 12:17:55 -0300

    feat/ajustado contexto do banco de dados

--- Todo.Backend/Context/TodoDbContext.cs
+++ Todo.Backend/Context/TodoDbContext.cs
@@ -14,6 +14,19 @@ public class TodoDbContext  : DbContext
 
     protected override void OnModelCreating(ModelBuilder mb)
     {
-
+        // Configuração da tabela ToDoTask
+        mb.Entity<ToDoTask>(entity =>
+        {
+            entity.ToTable("todoTasks");
+            
+            entity.HasKey(e => e.idTask);
+            
+            entity.Property(e => e.Description)
+                .IsRequired()
+                .HasMaxLength(30);
+            
+            entity.Property(e => e.Completed)
+                .HasDefaultValue(false);
+        });
     }
 }
