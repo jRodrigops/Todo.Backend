ddf1e0e6fb2526641e4358f8f95c5ab24d621f42
Jose <jose.silva@brisabr.com.br>
2025-11-22 12:19:03 -0300

    feat/ajustado padrao do mapper

--- Todo.Backend/Mapper/MappingProfile.cs
+++ Todo.Backend/Mapper/MappingProfile.cs
@@ -8,12 +8,14 @@ public class MappingProfile : Profile
 {
     public MappingProfile() 
     {
-        CreateMap<CreateToDoTaskRequestDTO, ToDoTask>();
+        CreateMap<CreateToDoTaskRequestDTO, ToDoTask>()
+            .ForMember(dest => dest.Completed, opt => opt.MapFrom(src => false));
         CreateMap<ToDoTask, CreateToDoTaskResponseDTO>();
         CreateMap<ToDoTask, ListToDoTaskResponseDTO>();
         CreateMap<UpdateDescriptionToDoTaskRequestDTO, ToDoTask>();
         CreateMap<ToDoTask, UpdateDescriptionToDoTaskResponseDTO>();
         CreateMap<UpdateStatusToDoTaskRequestDTO, ToDoTask>();
-        CreateMap<ToDoTask, UpdateStatusToDoTaskResponseDTO>();
+        CreateMap<ToDoTask, UpdateStatusToDoTaskResponseDTO>()
+            .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Completed));
     }
 }
